5cd329b4fcce3ab0350bde42abfe8eb1b2eba2ca
Ivan Gomes <ivanlourencogomes@gmail.com>
2024-01-23 12:59:54 +0000

    Fixed VAT bar hide and show logic to avoid CLS

--- js/products.js
+++ js/products.js
@@ -22,7 +22,6 @@ function displayProducts(products) {
         const img = document.createElement('img');
         img.src = product.image;
         img.alt = `product: ${product.title}`;
-        img.width=250;
         pictureDiv.appendChild(img);
 
         // Create the product info div
